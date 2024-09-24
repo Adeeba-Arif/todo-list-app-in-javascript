07b413a7d814e19df5ff7f09534a5da9531ddf3a
adeebachurail <adeebaarif1322@gmail.com>
2024-09-24 09:47:43 +0500

    styling added

---
+++ script.js
@@ -0,0 +1,7 @@
+function addTask(){
+    var input = document.getElementById("input")
+    var list = document.getElementById("list")
+    var li = document.createElement('li')
+    li.textContent = input.value
+    
+}
