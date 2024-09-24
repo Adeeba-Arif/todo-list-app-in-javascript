6971b1b7b1da56615bcf33b8b6dc22618b0b050e
adeebachurail <adeebaarif1322@gmail.com>
2024-09-24 10:29:06 +0500

    js file added

--- script.js
+++ script.js
@@ -3,5 +3,17 @@ function addTask(){
     var list = document.getElementById("list")
     var li = document.createElement('li')
     li.textContent = input.value
-    
+    li.className = "li"
+    var removeBtn = document.createElement('button')
+    removeBtn.textContent = 'remove'
+    removeBtn.className = 'rm-btn'
+    removeBtn.onclick = function () {
+        list.removeChild(li)
+    }
+    li.onclick = function () {
+        li.classList.toggle('completed')
+    }
+    li.appendChild(removeBtn)
+    list.appendChild(li)
+    input.value = ""
 }
