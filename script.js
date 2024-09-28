501bbc8145eaa9f41c8d666e80c2f789af8ad78a
Adeeba Arif <adeebaarif1322@gmail.com>
2024-09-28 10:57:27 +0500

    updated js file

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 function addTask(){
-    var input = document.getElementById("input")
-    var list = document.getElementById("list")
-    var li = document.createElement('li')
-    li.textContent = input.value
-    li.className = "li"
-    var removeBtn = document.createElement('button')
-    removeBtn.textContent = 'remove'
-    removeBtn.className = 'rm-btn'
+    var input = document.getElementById("input");
+    var list = document.getElementById("list");
+    var li = document.createElement('li');
+    li.textContent = input.value;
+    li.className = "li";
+    var removeBtn = document.createElement('button');
+    removeBtn.textContent = 'remove';
+    removeBtn.className = 'rm-btn';
     removeBtn.onclick = function () {
-        list.removeChild(li)
+        list.removeChild(li);
     }
     li.onclick = function () {
-        li.classList.toggle('completed')
+        li.classList.toggle('completed');
     }
-    li.appendChild(removeBtn)
-    list.appendChild(li)
-    input.value = ""
+    li.appendChild(removeBtn);
+    list.appendChild(li);
+    input.value = "";
 }
